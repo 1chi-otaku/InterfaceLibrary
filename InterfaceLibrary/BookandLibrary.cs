bcbbf92bd620b7a7e4798ec7351a7ba2fd2fba94
Serhii <mywaifuqueen@gmail.com>
2023-02-15 11:53:26 +0000

    Add project files.

---
+++ InterfaceLibrary/BookandLibrary.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace InterfaceLibrary
+{
+    class Book: IComparable, ICloneable
+    {
+        public string name { get; set; }
+        public string author { get; set; }
+
+        public Book() {
+            name = "N/A";
+            author = "N/A";
+        }
+        public Book(string name, string author)
+        {
+            this.name = name;
+            this.author = author;
+        }
+
+        public object Clone()
+        {
+            return new Book(name,author);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Book)
+                return name.CompareTo((obj as Book).name);
+
+            throw new NotImplementedException();
+        }
+        public void Show() => Console.WriteLine("{0}   |   {1}", name, author);
+
+        public class SortByName : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book && obj2 is Book)
+                    return (obj1 as Book).name.CompareTo((obj2 as Book).name);
+
+                throw new NotImplementedException();
+            }
+        }
+        public class SortByAuthor : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book && obj2 is Book)
+                    return (obj1 as Book).author.CompareTo((obj2 as Book).author);
+
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+    class Library: IEnumerable
+    {
+        Book[] book_list;
+
+        public Library() {
+            book_list = new Book[3];
+        }
+        public Library(params Book[] lists)
+        {
+            book_list = new Book[lists.Length];
+            for (int i = 0; i < lists.Length; i++)
+                book_list[i] = lists[i];
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < book_list.Length; i++)
+                yield return book_list[i];
+        }
+    }
+}
---
+++ InterfaceLibrary/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceLibrary
+{
+    internal class Program
+    {
+        static void Print(Book[] array)
+        {
+            Console.WriteLine("   Name                  Author");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(i + 1 + " - ");
+                array[i].Show();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[7];
+            books[0] = new Book("Once upon a time","R.Martin");
+            books[1] = new Book("An Amazing Book","An amazing Author");
+            books[2] = new Book("Bad book", "Tom");
+            books[3] = new Book("The ball", "Richard Tomson");
+            books[4] = new Book("The black swen", "S.T");
+            books[5] = new Book("Science for dummies","Enstein");
+            books[6] = new Book("Yahoo!", "Someone");
+            Console.WriteLine("Initial Array:");
+            Print(books);
+            Console.WriteLine();
+            Array.Sort(books, new Book.SortByName());
+            Console.WriteLine("Sorted by name:");
+            Print(books);
+            Console.WriteLine();
+            Array.Sort(books, new Book.SortByAuthor());
+            Console.WriteLine("Sorted by author:");
+            Print(books);
+            Console.WriteLine();
+            Book book2 = books[0].Clone() as Book;
+            books[0].name = "Name has been changed";
+            Console.WriteLine("Deep copy of the first book was created. First book name of the array was changed. Deep copy of the first book:");
+            book2.Show();
+            Console.WriteLine("Original:");
+            books[0].Show();
+            Console.WriteLine();
+            Console.WriteLine("Class 'Library' was created, Library via foreach:");
+            Library library= new Library(books);
+
+            int i = 1;
+            foreach (Book item in library)
+            {
+                Console.Write(i + " - ");
+                item.Show();
+                i++;
+            }
+        }
+    }
+}
